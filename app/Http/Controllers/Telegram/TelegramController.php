5043a032868e0fa62f417a90428771ec259bf5ab
Hosein <hoseinfordev@gmail.com>
2025-01-15 00:11:51 +0330

    Update TelegramController.php

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -42,7 +42,7 @@ class TelegramController extends Controller
 
 
         $JoinInfo = Telegram::getChatMember([
-            'chat_id' => '-2452640944',
+            'chat_id' => '2452640944',
             'user_id' => $this->GetUserInfo('id'),
         ]);
         if($JoinInfo['status'] != 'member' || $JoinInfo['status'] != 'creator' || $JoinInfo['status'] != 'administrator' ){
