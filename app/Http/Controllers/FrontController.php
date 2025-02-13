7d9ba3b1e7b2987bf0a17e43dc3cd671a952fc10
Hosein <hoseinfordev@gmail.com>
2025-02-13 21:28:34 +0330

    Update FrontController.php

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -8,7 +8,7 @@ class FrontController extends Controller
 {
     public function index()
     {
-        return view('Front.index');
+        return redirect()->route('Front.Games');
     }
     public function Games()
     {
