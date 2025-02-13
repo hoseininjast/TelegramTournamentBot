10e3ff5fc5f1a77197d57fcf1aae68245d7a6065
Hosein <hoseinfordev@gmail.com>
2025-02-13 22:11:23 +0330

    fix 1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,33 @@
 import './bootstrap';
 
-import './MainScript.js';
+import { init, backButton,closingBehavior,hapticFeedback , retrieveLaunchParams } from '@telegram-apps/sdk';
+
+
+const { initDataRaw, initData } = retrieveLaunchParams();
+const User = initData.user();
+
+$('#logs').text(User);
+$('#UserUsername').html('Welcome Back ' + User.username);
+$('#UserImage').src(User.photoUrl);
+
+init();
+backButton.mount();
+if (backButton.show.isAvailable()) {
+    backButton.show();
+}
+if (closingBehavior.mount.isAvailable()) {
+    closingBehavior.mount();
+    closingBehavior.isMounted(); // true
+}
+if (closingBehavior.enableConfirmation.isAvailable()) {
+    closingBehavior.enableConfirmation();
+    closingBehavior.isConfirmationEnabled(); // true
+}
+if (hapticFeedback.impactOccurred.isAvailable()) {
+    hapticFeedback.impactOccurred('medium');
+}
+
+const off = backButton.onClick(() => {
+    off();
+    window.history.back();
+});
