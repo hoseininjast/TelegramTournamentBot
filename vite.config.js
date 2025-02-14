53c8162258b4400bc8aed8320275b563f4afd4d1
Hosein <hoseinfordev@gmail.com>
2025-02-14 19:00:52 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/js/Front/Games.js',
                 'resources/js/Front/Tournaments.js',
