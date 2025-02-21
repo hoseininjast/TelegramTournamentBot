0f74cc735fd8cfbf3c36e1a61d8bc08f84bb715a
Hosein <hoseinfordev@gmail.com>
2025-02-21 23:12:11 +0330

    add jq to vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,47 +1,54 @@
-import { init, backButton,closingBehavior,hapticFeedback , retrieveLaunchParams } from '@telegram-apps/sdk';
+import './bootstrap';
+
+import { init, backButton,closingBehavior,hapticFeedback , retrieveLaunchParams , isTMA } from '@telegram-apps/sdk';
 
 
 
 
-const off = backButton.onClick(() => {
-    off();
-    window.history.back();
-});
 
 
 
-const { initDataRaw, initData } = retrieveLaunchParams();
 // const User = initData.user();
 
-$('#logs').text(initData);
-console.log(initData);
-console.log('----------');
-console.log('----------');
-console.log('----------');
-console.log(initDataRaw);
+
 // $('#UserUsername').html('Welcome Back ' + User.username);
 // $('#UserImage').src(User.photoUrl);
 
 
 
 window.addEventListener("DOMContentLoaded", async () => {
-
-    init();
-    backButton.mount();
-    if (backButton.show.isAvailable()) {
-        backButton.show();
-    }
-    if (closingBehavior.mount.isAvailable()) {
-        closingBehavior.mount();
-        closingBehavior.isMounted(); // true
-    }
-    if (closingBehavior.enableConfirmation.isAvailable()) {
-        closingBehavior.enableConfirmation();
-        closingBehavior.isConfirmationEnabled(); // true
-    }
-    if (hapticFeedback.impactOccurred.isAvailable()) {
-        hapticFeedback.impactOccurred('medium');
-    }
-
     $('#FooterBar').show();
+
+    if(isTMA()){
+        init();
+        const { initDataRaw, initData } = retrieveLaunchParams();
+
+        console.log(initData);
+        console.log('----------');
+        console.log('----------');
+        console.log('----------');
+        console.log(initDataRaw);
+
+        backButton.mount();
+        if (backButton.show.isAvailable()) {
+            backButton.show();
+        }
+        if (closingBehavior.mount.isAvailable()) {
+            closingBehavior.mount();
+            closingBehavior.isMounted(); // true
+        }
+        if (closingBehavior.enableConfirmation.isAvailable()) {
+            closingBehavior.enableConfirmation();
+            closingBehavior.isConfirmationEnabled(); // true
+        }
+        if (hapticFeedback.impactOccurred.isAvailable()) {
+            hapticFeedback.impactOccurred('medium');
+        }
+        const off = backButton.onClick(() => {
+            off();
+            window.history.back();
+        });
+    }
+
+
 });
