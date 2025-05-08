3dbcf5beb16f620ab75aa153006026cc5a444de7
Hosein <hoseinfordev@gmail.com>
2025-05-08 21:06:37 +0330

    Update Header.blade.php

--- resources/views/layouts/Dashboard/Header.blade.php
+++ resources/views/layouts/Dashboard/Header.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar-custom">
 
     <!-- LOGO -->
-    <div class="logo-box">
+   {{-- <div class="logo-box">
         <a href="{{route('Dashboard.index')}}" class="logo logo-light text-center">
                             <span class="logo-sm">
                                 <img src="{{asset('images/MainLogo.png')}}" alt="" height="22">
@@ -18,14 +18,20 @@
                                 <img src="{{asset('images/MainLogo.png')}}" alt="" height="16">
                             </span>
         </a>
-    </div>
+    </div>--}}
 
     <ul class="list-unstyled topnav-menu topnav-menu-left mb-0">
+        <li>
+            <button class="button-menu-mobile disable-btn waves-effect" onclick="history.back()">
+                <i class="fa fa-arrow-left"></i>
+            </button>
+        </li>
         <li>
             <button class="button-menu-mobile disable-btn waves-effect">
                 <i class="fe-menu"></i>
             </button>
         </li>
+
 
         <li>
             <h4 class="page-title-main">Dashboard</h4>
